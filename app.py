c0e64240fa07ba50b32449adfe7a0f0702f092dc
Brad Haas <bradh@presidio.com>
2019-03-02 08:14:38 -0500

    add release notes

--- app.py
+++ app.py
@@ -35,8 +35,10 @@ webhook_listener = webhook_listener_base_url + f":{webhook_port}/{bot_name}"
 
 app = Flask(__name__)
 
-help_message_group = f"## Webex Teams Update Notifier\nThank you for adding me to your space.  I am here to alert you when new versions of Webex Teams are released by Cisco.  I will do this automatically unless you ask me not to.\n\n* If you want to stop receiving automatic updates simply @mention me and type `unsubscribe`.\n\n* If you want to opt back in simply @mention me and type `subscribe`"
-help_message_direct = f"## Webex Teams Update Notifier\nThank you for adding me to your space.  I am here to alert you when new versions of Webex Teams are released by Cisco.  I will do this automatically unless you ask me not to.\n\n* If you want to stop receiving automatic updates simply type `unsubscribe`.\n\n* If you want to opt back in simply type `subscribe`"
+help_message_group = f"## Webex Teams Update Notifier\nThank you for adding me to your space.  I am here to alert you when new versions of Webex Teams are released by Cisco.  I will do this automatically unless you ask me not to.\n\n* If you want to stop receiving automatic updates simply @mention me and type `unsubscribe`.\n\n* If you want to opt back in simply @mention me and type `subscribe`\n\nIf you want to know the latest version, simply type `version`"
+help_message_direct = f"## Webex Teams Update Notifier\nThank you for adding me to your space.  I am here to alert you when new versions of Webex Teams are released by Cisco.  I will do this automatically unless you ask me not to.\n\n* If you want to stop receiving automatic updates simply type `unsubscribe`.\n\n* If you want to opt back in simply type `subscribe`\n\nIf you want to know the latest version, simply type `version`"
+release_notes = f"https://help.webex.com/en-us/mqkve8/Cisco-Webex-Teams-Release-Notes"
+whats_new = f"https://help.webex.com/en-us/8dmbcr/What-s-New-in-Cisco-Webex-Teams"
 
 
 def register_webhook():
@@ -95,6 +97,7 @@ def latest_version_message(version_info):
             f"The latest version of Webex Teams for {platform} is **{version}**"
         )
         messages.append(message_body)
+    messages.append(f"Release notes can be found [here]({release_notes})")
     return messages
 
 
